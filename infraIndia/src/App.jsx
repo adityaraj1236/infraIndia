71fed8000033a5af03f175bb8210d4e184264527
Your Name <adityaraj642003@gmail.com>
2025-03-07 00:39:26 +0530

    uodated v5

--- infraIndia/src/App.jsx
+++ infraIndia/src/App.jsx
@@ -1,10 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1>Hello Jee kaise ho saare  sab</h1>
-      <h2> we will gonna rock the construction world with our this project</h2>
-      <h3> hope you like it</h3>
-      <p>lorem ipsum dolor sit amet consectetur adipisicing elit .git adipisicing elit</p>
+      <p>Hello We are starting off the best cnstrution management project ever </p>
     </div>
   )
 }
