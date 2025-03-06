1b9dcc5ec03c9af7be84dd21679b58585ceaa070
Your Name <adityaraj642003@gmail.com>
2025-03-07 00:46:15 +0530

    coming soon message displayed

--- infraIndia/src/App.jsx
+++ infraIndia/src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen text-white text-3xl text-center font-bold bg-gray-900">
+    <div className="flex flex-col items-center justify-center h-screen text-white text-3xl text-center font-bold bg-gray-900 w-full">
       <h1>InfraIndia</h1>
       <p>COMING SOON!</p>
     </div>
