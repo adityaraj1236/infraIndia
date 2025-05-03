55111df46b8fa5eae9f15e3ec59bc2511355ab33
Your Name <adityaraj642003@gmail.com>
2025-05-03 16:10:24 +0530

    name changed

--- infraIndia/src/components/Navbar/Navbar.jsx
+++ infraIndia/src/components/Navbar/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center p-4 bg-white shadow-md w-full">
       {/* Logo */}
-      <div className="text-lg font-bold text-gray-800">InfraIndia</div>
+      <div className="text-lg font-bold text-gray-800 space-x-4">Xpans<span className="text-blue-600">Hil</span><span className="space-x-1">Global</span></div>
 
       {/* Desktop navigation (hidden on mobile) */}
       <div className="hidden md:flex gap-6 items-center">
